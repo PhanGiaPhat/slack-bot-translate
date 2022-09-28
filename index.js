e35505694e4ed4de96874fac868dfccbe1daf08b
PhanGiaPhat <phat@oven.bz>
2022-09-28 12:17:27 +0700

    change token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { App } = require("@slack/bolt");
 
 
 const app = new App({
-    token: "xoxb-810419405043-4124002799878-3GvYPyp4fY9yMsz5tofhux1E", //Find in the Oauth  & Permissions tab
+    token: "xoxb-810419405043-4124002799878-955spagPL9SZZxxPtECezCmZ", //Find in the Oauth  & Permissions tab
     signingSecret: "ea5ce3685a280fbcb5bd4591a64bf237", // Find in Basic Information Tab
     socketMode:true,
     appToken: "xapp-1-A043UJ51FB4-4140271409252-042a8e7a0bbb1908c12c0bcc9e24fde811d0df9cf85a19c604b4e654b5243e72" // Token from the App-level Token that we created
