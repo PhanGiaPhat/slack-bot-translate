fd54bd3d429598c27c22b00e5741947c58edcfae
PhanGiaPhat <phat@oven.bz>
2022-09-29 17:31:40 +0700

    slash command

--- index.js
+++ index.js
@@ -8,28 +8,85 @@ const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     socketMode:true, // enable the following to use socket mode
-    appToken: process.env.SLACK_APP_TOKEN
+    appToken: process.env.SLACK_APP_TOKEN,
+    lang: 'en'
   });
 
 // Listener middleware that filters out messages with 'bot_message' subtype
     async function noBotMessages({ message, next }) {
-        if (!message.subtype || message.subtype !== 'bot_message') {
+        console.log(message.subtype)
+        if (!message.subtype || message.subtype !== "") {
         await next();
         }
     }
 
   // The listener only receives messages from humans
+
   app.message(noBotMessages, async ({ client, logger, message }) => {
     try {
         client.chat.postMessage({
             channel: message.channel,
-            text: (await translate(message.text, {to: 'en'})).text,
-            as_user: true,
+            text: (await translate(message.text, {to: app.lang})).text,
+            thread_ts: message.ts
         })
       }
       catch (error) {
         logger.error(error);
       }
+  });
+
+  app.command("/heybot", async ({
+    command,
+    ack
+  }) => {
+    await ack()
+    await app.client.conversations.join({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: command.channel_id, 
+    });
+    app.client.chat.postMessage({
+        channel: command.channel_id,
+        text: "I'm here to help you translate your messages",
+    })
+  });
+
+  app.command("/byebot", async ({
+    command,
+    ack
+  }) => {
+    app.client.chat.postMessage({
+        channel: command.channel_id,
+        text: "See you! I'm left",
+    })
+    await ack()
+    await app.client.conversations.leave({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: command.channel_id,
+    });
+  });
+
+  app.command("/addlang", async ({
+    command,
+    ack
+  }) => {
+    app.lang = `${command.text}`
+    await ack()
+    await app.client.chat.postMessage({
+        channel: command.channel_id,
+        text: "Trasnlate to "+app.lang,
+    })
+  });
+
+  app.command("/byelang", async ({
+    command,
+    ack
+  }) => {
+    app.lang = 'en'
+    await ack()
+    await app.client.chat.postMessage({
+        channel: command.channel_id,
+        text: "Trasnlate to en",
+    })
   });
 
 (async () => {
